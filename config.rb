a751c67510dd2f8ba5f5ede73013aaba292d382b
togo-mentor <uint2435@gmail.com>
2022-08-13 06:47:49 +0900

    各種設定

--- config.rb
+++ config.rb
@@ -5,10 +5,10 @@ require './qiita_notifier/configuration'
 
 QiitaNotifier.configure do |config|
   # Qiitaでログイン出来るユーザー名とパスワードをセットしてください
-  config.user_name = 'user_name'
-  config.password = 'password'
+  config.user_name = ENV['QIITA_USER']
+  config.password = ENV['QIITA_PASSWORD']
   # Slackで作成したアプリのWebHookURLを設定してください
-  config.web_hook_url = 'web_hook_url'
+  config.web_hook_url = ENV['WEBHOOK_URL']
 end
 
 QiitaTrend.configure do |config|
